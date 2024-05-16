9f17471a4a31fc328d3c30377c8e431db44b5035
Xudoberdi1107 <tojiboyevxudoberdi1107@gmail.com>
2024-05-17 02:19:39 +0500

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 
 function App() {
@@ -8,13 +7,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
+        <a href="https://react.dev" target="_blank"></a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
