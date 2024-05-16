17c7a4df25a35e5fd00259b19cb59b2f136b0c6c
Xudoberdi1107 <tojiboyevxudoberdi1107@gmail.com>
2024-05-17 01:49:00 +0500

    dev

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+const { EndOfLineState } = require('typescript');
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': [
+      'warn',
+      {
+        EndOfLine: 'auto',
+      },
+    ],
+    'prefer-const': 'warn',
+    'no-var': 'error',
+  },
+};
